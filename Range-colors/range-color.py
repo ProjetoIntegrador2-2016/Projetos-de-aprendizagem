0af71eeb9c215419e038cc45ed807144d697ea2a
DaniloBarros <dan.b412@gmail.com>
2016-09-28 17:20:01 -0300

    Addding Object Tracking to test HSV values

--- Range-colors/range-color.py
+++ Range-colors/range-color.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('test2.jpg')
+img = cv2.imread('CalibrateColor.jpg')
 
-purple_min = np.array([3,51,40],np.uint8)
-purple_max = np.array([13,255,255],np.uint8)
+purple_min = np.array([3, 51, 40], np.uint8)
+purple_max = np.array([13, 255, 255], np.uint8)
 
-hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+green_min = np.array([27, 68, 38], np.uint8)
+green_max = np.array([88, 255, 255], np.uint8)
 
-frame_threshed = cv2.inRange(hsv_img, purple_min, purple_max)
-cv2.imwrite('result.jpg', frame_threshed)
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+frame_threshed = cv2.inRange(hsv_img, green_min, green_max)
+cv2.imwrite('result1.jpg', frame_threshed)
