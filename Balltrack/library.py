039471ac450ae120cd8b4f927a281eab0c267bc2
DaniloBarros <dan.b412@gmail.com>
2016-10-01 12:26:42 -0300

    Added constants, conformed style and added return largestContour to findCircleContour function

--- Balltrack/library.py
+++ Balltrack/library.py
@@ -4,21 +4,24 @@ import cv2
 COLOR = (0, 0, 255)
 THICKNESS = 2
 
+
 def applyMasks(frame, greenUpperBound, greenLowerBound):
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # creates a mask for the color, do dilatations and erosions
-	# to remove small blobs in the mask
-	mask = cv2.inRange(hsv, greenLowerBound, greenUpperBound)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-	return mask
+    # to remove small blobs in the mask
+    mask = cv2.inRange(hsv, greenLowerBound, greenUpperBound)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    return mask
+
 
 def findCircleContour(contours):
-	#find the largest contour in the mask, then use it to compute
-	# the minimunm enclosing circle and centroid
-	largestContour = max(contours, key = cv2.contourArea)
-	((x,y),radius) = cv2.minEnclosingCircle(largestContour)
-	return (x,y), radius
+    # find the largest contour in the mask, then use it to compute
+    # the minimunm enclosing circle and centroid
+    largestContour = max(contours, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(largestContour)
+    return (x, y), radius, largestContour
+
 
 def calculateCentroid(largestContour):
     M = cv2.moments(largestContour)
@@ -26,11 +29,13 @@ def calculateCentroid(largestContour):
         centroid = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         return centroid
 
+
 def drawCircle(frame, center, radius):
     cv2.circle(frame, (int(center[0]), int(center[1])), int(radius), COLOR, THICKNESS)
 
+
 def findScreenPosition(centroid, frame):
-    position =""
+    position = ""
 
     if centroid[0] < (frame.shape[1] / 3):
         position = "Esquerda"
@@ -48,9 +53,11 @@ def findScreenPosition(centroid, frame):
 
     return position
 
+
 def printDistance(frame, distance):
-	cv2.putText(frame, "%.2f cm" % distance, (frame.shape[1] - 200, frame.shape[0] - 20),
+    cv2.putText(frame, "%.2f cm" % distance, (frame.shape[1] - 200, frame.shape[0] - 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.0, COLOR, THICKNESS)
+
 
 def printPosition(frame, position):
     cv2.putText(frame, position, (50, frame.shape[0] - 20),
