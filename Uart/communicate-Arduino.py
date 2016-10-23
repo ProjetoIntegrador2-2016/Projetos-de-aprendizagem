668b83eedefc8f79589e94ca42a994046f6cb06c
DaniloBarros <dan.b412@gmail.com>
2016-10-23 15:34:07 +0000

    Communication raspbery-arduino uno

--- Uart/communicate-Arduino.py
+++ Uart/communicate-Arduino.py
@@ -1,16 +1,12 @@
 import serial
 import time
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
 #ser.open() # serial is already open
 
 # Variable dir will be set according to the 
 # direction that the prototype needs to go
 # based on the target action
 
-dirR = 'r' #right -> ascii character
-dirL = 'l' #left
-foward = 'f' 
-back = 'b'
 
 try:
  while 1:
