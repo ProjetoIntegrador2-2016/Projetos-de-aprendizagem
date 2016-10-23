0a762b73b04c797134a2ff22ea18381c0bb2dafc
DaniloBarros <dan.b412@gmail.com>
2016-10-23 13:43:41 -0200

    Updated Arduino to control motors based on rapberry input

--- Uart/communication-Rasp/communication-Rasp.ino
+++ Uart/communication-Rasp/communication-Rasp.ino
@@ -1,36 +1,100 @@
-// Programa: Comunicacao Arduino com Raspberry Pi
-// Autor: FILIPEFLOP
- 
-const int pino_led = 13;
-char buf;
-int estado = 1;
- 
+// Clockwise and counter-clockwise definitions.
+// Depending on how you wired your motors, you may need to swap.
+#define CW  0
+#define CCW 1
+
+// Motor definitions to make life easier:
+#define MOTOR_A 0
+#define MOTOR_B 1
+
+#define STOP 's'
+#define FOWARD 'f'
+#define BACKWARD 'b'
+#define LEFT 'l'
+#define RIGHT 'r'
+
+const byte PWMA = 3;  // PWM control (speed) for motor A
+const byte PWMB = 11; // PWM control (speed) for motor B
+const byte DIRA = 12; // Direction control for motor A
+const byte DIRB = 13; // Direction control for motor B
+
+char incoming = 'q';
+
 void setup()
 {
-  pinMode(pino_led, OUTPUT);
-  Serial.begin(115200);
+  Serial.begin(9600);
+  setupArdumoto();
 }
- 
+
 void loop()
 {
-  while (Serial.available() > 0)
-  {
-    buf = Serial.read();
-    // Caso seja recebido o caracter L, acende o led
-    if (buf == 'L')
-    {
-      // Liga ou desliga o led da porta 13
-      digitalWrite(pino_led, estado);
-      // Envia a resposta para o Raspberry
-      Serial.print("Recebido! - Estado Led: ");
-      if (estado == 1)
-      {
-        Serial.println("Ligado");
-      }
-      else
-        Serial.println("Desligado");
+  
+  if (Serial.available()){
+    
+    incoming = Serial.read();
+    switch(incoming){
+      case 'f':
+        driveArdumoto(MOTOR_A, CCW, 100);
+        driveArdumoto(MOTOR_B, CCW, 100);
+      break;
+      case 'b':
+        driveArdumoto(MOTOR_A, CW, 100);
+        driveArdumoto(MOTOR_B, CW, 100);
+      break;
+      case 'l':
+        analogWrite(PWMA, 50);
+        analogWrite(PWMB, 255);
+      break;
+      case 'r':
+        analogWrite(PWMA, 255);
+        analogWrite(PWMB, 50);
+      break;
+      default:
+        stopArdumoto(MOTOR_A);
+        stopArdumoto(MOTOR_B);
     }
-    // Inverte o estado do led
-    estado = !estado;
+    delay(1000);
+    if(incoming != 'q'){
+      Serial.write(incoming);
+    }
+    delay(1000);
+    
   }
+}
+
+// driveArdumoto drives 'motor' in 'dir' direction at 'spd' speed
+void driveArdumoto(byte motor, byte dir, byte spd)
+{
+  if (motor == MOTOR_A)
+  {
+    digitalWrite(DIRA, dir);
+    analogWrite(PWMA, spd);
+  }
+  else if (motor == MOTOR_B)
+  {
+    digitalWrite(DIRB, dir);
+    analogWrite(PWMB, spd);
+  }  
+}
+
+// stopArdumoto makes a motor stop
+void stopArdumoto(byte motor)
+{
+  driveArdumoto(motor, 0, 0);
+}
+
+// setupArdumoto initialize all pins
+void setupArdumoto()
+{
+  // All pins should be setup as outputs:
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+
+  // Initialize all pins as low:
+  digitalWrite(PWMA, LOW);
+  digitalWrite(PWMB, LOW);
+  digitalWrite(DIRA, LOW);
+  digitalWrite(DIRB, LOW);
 }
